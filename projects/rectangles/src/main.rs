be28f963ee8192551b28e8d3950ed5182b86a4a0
mozy <rnjs213@gmail.com>
2022-11-08 01:25:33 +0900

    tutorial 5.3 메소드 문법

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,12 +4,29 @@ struct Rectangle {
     width: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { length: 50, width: 30 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
 
-    println!("rect1 is {:#?}", rect1);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {length: size, width: size}
+    }
 }
 
-fn area(rectangle : Rectangle) -> u32 {
-    rectangle.length * rectangle.width
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+
+    println!("sq is {:#?}", sq);
 }
