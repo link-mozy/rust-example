8455b4f7d36eea3b244749429228a29c7db2ff70
link-mozy <rnjs213@gmail.com>
2023-07-25 08:04:07 +0900

    만들면서 배우는 러스트 프로그래밍 - 소유권 돌려주기

---
+++ rust-programming-learn-while-building/source/ch03/owner_func_ret.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut g1 = String::from("실수할 줄 아는 사람이 아름답다");
+    g1 = show_message(g1); // 소유권이 이동한다.
+    println!("{}", g1); // g1은 사용할 수 없다.
+}
+
+fn show_message(message: String) -> String {
+    println!("{}", message);
+    message
+}
